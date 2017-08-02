9d4e629ee12317149801dce4653ebb82cabb8709
Wellington Lopes <wellington.lopes.tw@gmail.com>
2017-08-01 23:25:58 -0400

    aula 04

---
+++ aula04/tipos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/estilo.css"/>
+		<meta charset="UTF-8"/>
+		<title></title>
+	</head>
+	<body>
+		<div>
+			<?php
+				$idade = 20;
+				$nome = "Wellington";
+				#$idade = 20.4; #irá mudar o tipo para double
+				echo "$nome tem $idade anos!";#com '' ele não reconhece as variaveis
+			?>
+		</div>
+	</body>
+</html>
